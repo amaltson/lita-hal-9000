9ab4af09706133db31ce1c0879bf2c25f0eb5372
Arthur Maltson <arthur@maltson.com>
2014-11-17 13:38:22 -0500

    Instantiate source with whether a private method

--- lib/lita/handlers/hal_9000.rb
+++ lib/lita/handlers/hal_9000.rb
@@ -4,7 +4,7 @@ module Lita
       on(:unhandled_message) do |payload|
         message = payload[:message]
         return unless message.command?
-        target = Source.new(room: payload[:room])
+        target = Source.new(room: payload[:room], private_message: payload[:private_message])
         robot.send_message(target, "I'm sorry, I can't do that #{message.user}. http://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/HAL9000.svg/200px-HAL9000.svg.png")
       end
     end
