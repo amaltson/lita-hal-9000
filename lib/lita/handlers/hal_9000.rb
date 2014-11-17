fb76f64a3604251e86406fe5d9839cfaa02ebe9e
Arthur Maltson <arthur@maltson.com>
2014-11-17 13:22:40 -0500

    Initial commit with the message
    This needs tests, to be a separate method, etc.

--- lib/lita/handlers/hal_9000.rb
+++ lib/lita/handlers/hal_9000.rb
@@ -1,6 +1,12 @@
 module Lita
   module Handlers
     class Hal9000 < Handler
+      on(:unhandled_message) do |payload|
+        message = payload[:message]
+        return unless message.command?
+        target = Source.new(room: payload[:room])
+        robot.send_message(target, "I'm sorry, I can't do that #{message.user}. http://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/HAL9000.svg/200px-HAL9000.svg.png")
+      end
     end
 
     Lita.register_handler(Hal9000)
