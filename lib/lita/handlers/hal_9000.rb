3fb79dbb4103ce019b99a4dd14839bf4764bc8b2
Arthur Maltson <arthur@maltson.com>
2014-11-17 13:57:24 -0500

    Different instantiation for PM or room

--- lib/lita/handlers/hal_9000.rb
+++ lib/lita/handlers/hal_9000.rb
@@ -3,9 +3,15 @@ module Lita
     class Hal9000 < Handler
       on(:unhandled_message) do |payload|
         message = payload[:message]
-        return unless message.command?
-        target = Source.new(room: payload[:room], private_message: message.source.private_message)
-        robot.send_message(target, "I'm sorry, I can't do that #{message.user}. http://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/HAL9000.svg/200px-HAL9000.svg.png")
+        if message.command?
+          private_message = message.source.private_message
+          target = unless private_message
+                     Source.new(room: message.source.room)
+                   else
+                     Source.new(user: message.user, private_message: true)
+                   end
+          robot.send_message(target, "I'm sorry, I can't do that #{message.user.mention_name}. http://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/HAL9000.svg/200px-HAL9000.svg.png")
+        end
       end
     end
 
