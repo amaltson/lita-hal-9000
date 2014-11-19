e2ecc6fdd748d1e4ea245cd7d965fb7344a7c77e
Arthur Maltson <arthur@maltson.com>
2014-11-19 09:07:56 -0500

    Add test for sending unknown command

--- spec/lita/handlers/hal_9000_spec.rb
+++ spec/lita/handlers/hal_9000_spec.rb
@@ -2,4 +2,10 @@ require "spec_helper"
 
 describe Lita::Handlers::Hal9000, lita_handler: true do
   it { is_expected.to route_event(:unhandled_message) }
+
+  it 'does HAL message if unknown command' do
+    send_command('unknown-command')
+    expect(replies.last).to eq(
+      "I'm sorry, I can't do that @Test User http://bit.ly/11wwIP2")
+  end
 end
