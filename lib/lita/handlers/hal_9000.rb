9fac07d60e9bf660cee7e885f58f96b0bd550244
Arthur Maltson <arthur@maltson.com>
2014-11-19 11:58:17 -0500

    Refactor handling message into method

--- lib/lita/handlers/hal_9000.rb
+++ lib/lita/handlers/hal_9000.rb
@@ -2,6 +2,10 @@ module Lita
   module Handlers
     class Hal9000 < Handler
       on(:unhandled_message) do |payload|
+        handle_unhandled(payload: payload)
+      end
+
+      def handle_unhandled(payload:)
         message = payload[:message]
         if message.command?
           private_message = message.source.private_message
@@ -13,6 +17,7 @@ module Lita
           robot.send_message(target, "I'm sorry, I can't do that @#{message.user.mention_name} http://bit.ly/11wwIP2")
         end
       end
+
     end
 
     Lita.register_handler(Hal9000)
