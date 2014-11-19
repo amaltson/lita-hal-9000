18f49127b6590dae67ef832418b0e84e2900a0ac
Arthur Maltson <arthur@maltson.com>
2014-11-19 09:36:18 -0500

    Add test to ensure mention name used

--- spec/lita/handlers/hal_9000_spec.rb
+++ spec/lita/handlers/hal_9000_spec.rb
@@ -8,4 +8,12 @@ describe Lita::Handlers::Hal9000, lita_handler: true do
     expect(replies.last).to eq(
       "I'm sorry, I can't do that @Test User http://bit.ly/11wwIP2")
   end
+
+  it 'does HAL message using mention name' do
+    john = Lita::User.create(123, name: 'John Smith', mention_name: 'john')
+    send_command('unknown-command', as: john)
+    expect(replies.last).to eq(
+      "I'm sorry, I can't do that @john http://bit.ly/11wwIP2")
+  end
+
 end
